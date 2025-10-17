4c28cda72518917af406d5a88fdd68d6f7c1cf5d
ItsJustOlivr <87696598+ItsJustOlivr@users.noreply.github.com>
2025-10-17 18:33:26 +0100

    Added main Python file.
    Also added json files. These are the puzzles that will be read and displayed onto the GUI.

---
+++ binairo.py
@@ -0,0 +1,17 @@
+import csv
+import tkinter as tk
+from tkinter import filedialog, messagebox
+
+
+class BinairoGUI:
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.title("Binairo Puzzle Solver")
+
+
+
+
+
+
+class BinairoPuzzle:
+    
